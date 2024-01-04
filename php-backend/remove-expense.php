9c8ce1b4fc1e1acc0153617f8066466c9404995b
Šimon Ukuš <ukussimon2@gmail.com>
2024-01-04 22:26:08 +0100

    Implement deleting + prepare UI for filtering
    Signed-off-by: Šimon Ukuš <ukussimon2@gmail.com>

---
+++ php-backend/remove-expense.php
@@ -0,0 +1,29 @@
+<?php
+
+require 'vendor/autoload.php';
+
+require __DIR__ . '/dbactions/delete-expense.php';
+
+require __DIR__ . '/session/session.php';
+
+use Ramsey\Uuid\Uuid;
+
+// SUBMIT EXPENSE
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $expense_id = $_POST['expense_id'];
+
+    header('Content-Type: application/json');
+
+    // just perform the deletion
+    // ideally we would check the cookie and verify the session and also if the 
+    // given expense is associated with that user, but in this simple app we dont care
+    delete_expense($expense_id);
+
+    http_response_code(204);
+    $response = [
+        'status' => 'resource deleted successfully',
+        'code' => 204,
+        'message' => 'Expense deleted successfully',
+    ];
+    echo json_encode($response);
+}
--- svelte-frontend/src/components/UserExpenses.svelte
+++ svelte-frontend/src/components/UserExpenses.svelte
@@ -1,19 +1,45 @@
 <script>
     import UserExpense from './UserExpense.svelte';
   
+    let orderings = ['category ↓', 'category ↑', 'amount ↓', 'amount ↑', 'date ↓', 'date ↑'];
+    let ordering = orderings[3];
+
     export let expenses;
+
+    function removeExpense(event) {
+        console.log({'pre-remove': expenses});
+        expenses = expenses.filter(exp => exp != event.detail.expense);
+        console.log({'post-remove': expenses});
+    }
 
 </script>
 
 <div class="user-expenses">
     <h2>User Expenses</h2>
+    <label for="">
+        <select bind:value={ordering}>
+            {#each orderings as ord (ord)}
+            <option value={ord}>{ord}</option>
+            {/each}
+        </select>
+    </label>
     {#each expenses as expense (expense.expense_id)}
-        <UserExpense {expense} />
+        <UserExpense {expense} on:expenseDeleted={removeExpense}/>
     {/each}
 </div>
 
 <style>
     .user-expenses {
         margin-top: 20px;
+    }
+    
+    select {
+        width: 100%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        border: none;
+        border-radius: 4px;
+        box-sizing: border-box;
+        background-color: #f2f2f2;
     }
 </style>
